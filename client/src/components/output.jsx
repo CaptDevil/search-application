673bde0a8e5addb1888e83e411a917500f4aa235
CaptDevil <prabhudutta.sarangi@gmail.com>
2021-03-04 21:36:11 +0530

    project completed

--- client/src/components/output.jsx
+++ client/src/components/output.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 function Output(props) {
     let button;
+    console.log(props)
     if(props.data.time>0)
         button=<p>Searched from {props.data.count} results in {props.data.time} ns.</p>
     else
