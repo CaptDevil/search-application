d1dc5c6ce764e56578aa5e3b3eab233fcf67293e
CaptDevil <prabhudutta.sarangi@gmail.com>
2021-03-04 09:38:17 +0530

    Final commit

--- client/src/components/output.jsx
+++ client/src/components/output.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 
 function Output(props) {
+    let button;
+    if(props.data.time==0)
+        button=<p>Searched in {props.data.time} ns, from {props.data.count} results.</p>
+    else
+        button=<p></p>
     return (
         <div>
-            <p>Searched in {props.data.time} ns, from {props.data.count} results.</p>
+            {button}
             <ul>
                 {props.data.results.map(a=><li key={parseInt(Math.random()*1000)}>{a}</li>)}
             </ul>
