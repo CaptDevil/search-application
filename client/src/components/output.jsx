8cd41e1f52289b28d829cfd43c6ab78aa59334ea
CaptDevil <prabhudutta.sarangi@gmail.com>
2021-03-04 21:49:45 +0530

    project completed

--- client/src/components/output.jsx
+++ client/src/components/output.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function Output(props) {
-    let button;
+    let button,i=0;
     console.log(props)
     if(props.data.time>=0 && props.data.count>=0)
         button=<p>Searched from {props.data.count} results in {props.data.time} ns.</p>
@@ -11,7 +11,7 @@ function Output(props) {
         <div>
             {button}
             <ul>
-                {props.data.results.map(a=><li key={parseInt(Math.random()*1000)}>{a}</li>)}
+                {props.data.results.map(a=><li key={i++}>{a}</li>)}
             </ul>
         </div>
     );
