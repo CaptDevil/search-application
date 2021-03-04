be65ea928f247b26fffbe5e2adc43b3e7bcc2914
CaptDevil <prabhudutta.sarangi@gmail.com>
2021-03-04 09:27:40 +0530

    Final commit

--- client/src/components/output.jsx
+++ client/src/components/output.jsx
@@ -3,8 +3,7 @@ import React from 'react';
 function Output(props) {
     return (
         <div>
-            <p>Searched in {props.data.time} ns</p>
-            <p>From {props.data.count} datas</p>
+            <p>Searched in {props.data.time} ns, from {props.data.count} datas</p>
             <ul>
                 {props.data.results.map(a=><li key={parseInt(Math.random()*1000)}>{a}</li>)}
             </ul>
