b66a7f20cd599ca9552d052fda6ac3cdce592327
CaptDevil <prabhudutta.sarangi@gmail.com>
2021-03-06 22:37:16 +0530

    added support for history of results to reduce the searching time

--- app.js
+++ app.js
@@ -73,10 +73,11 @@ function partition(low,high,A){
     pivot=low,i=low+1,j=high
     while(i<=j){
         console.log(A[pivot],A[i],A[j])
-        while(A[pivot].localeCompare(A[i])>=0)
+        while(A[pivot].localeCompare(A[i])>=0 && i!=j)
             i++;
         while(A[pivot].localeCompare(A[j])<0)
             j--;
+        
         if(i<=j)
             swapArr(i,j,A)
     }
