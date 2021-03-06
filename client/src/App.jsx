db88c3d9292ac63ef5169c511c510d7f92019bb0
CaptDevil <prabhudutta.sarangi@gmail.com>
2021-03-07 00:15:21 +0530

    added support for history of results to reduce the searching time

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -16,7 +16,7 @@ class App extends Component {
       .then(res=>{
         this.setState({time:res.data.time,count:res.data.count,results:res.data.arr})
       })
-    console.log(data)
+    console.log(typeof data)
     /**
      * if(data){
       
